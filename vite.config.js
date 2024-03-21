380731d1deb8f3a430ee41bda9376fb144e23161
Shubham Kumar <shubham.kr3112@gmail.com>
2024-03-21 22:11:45 +0530

    cors update

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import http from "https";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server: {
-  //   proxy: {
-  "/api": "https://backendwa-j4ck.onrender.com",
-  //   },
-  // },
+  server: {
+    proxy: {
+      "/api": "https://backendwa-j4ck.onrender.com",
+    },
+  },
   plugins: [react()],
 });
