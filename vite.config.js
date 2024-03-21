95beb0e490c1800b7439cc43068ee23df5cb900a
Shubham Kumar <shubham.kr3112@gmail.com>
2024-03-21 10:18:15 +0530

    second commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "https://backendwa-j4ck.onrender.com/",
+      "/api": "https://backendwa-j4ck.onrender.com",
     },
   },
   plugins: [react()],
